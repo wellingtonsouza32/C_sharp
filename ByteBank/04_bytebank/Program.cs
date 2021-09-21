25c51de392659ebfb1ea1a7c23ff4c4b3a156e24
wellington.souza <wellington.souza@totvs.com.br>
2021-09-21 00:13:45 -0300

    atualização bytebank 04

---
+++ ByteBank/04_bytebank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+            Console.WriteLine(contaDoBruno.saldo);
+            contaDoBruno.Sacar(50);
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine(resultadoSaque);
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno:" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela:" + contaDaGabriela.saldo);
+
+            bool resultadodaTransferencia = contaDoBruno.Transferir(2000, contaDaGabriela);
+            contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno:" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela:" + contaDaGabriela.saldo);
+            Console.ReadLine();
+        }
+    }
+}
